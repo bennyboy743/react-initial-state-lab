9e7bbd237801abb896a2511de89f541d279efdf6
Benjamin <benjaminrowe0@gmail.com>
2022-03-02 19:29:40 +1100

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,30 @@
 // your Bomb code here!
+import React, { Component } from "react";
+
+export default class Bomb extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            count: props.initialCount
+        }
+
+    }
+    render(){
+        return (
+            <div>
+               {this.state.count >= 0 ?  <p>{this.state.count} seconds left before I go boom!</p> : <p>BOOM</p>} 
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        this.decreaseTime = setInterval(() => {
+            this.setState({
+                count: this.state.count - 1
+            })
+        },1000)
+    }
+    componentWillUnmount(){
+        clearInterval(this.decreaseTime)
+    }
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,26 @@
 // your ImageSlider code here!
+import React, { Component } from "react";
+
+export default class ImageSlider extends Component {
+    constructor() {
+        super()
+        this.state = {
+            currentSlideIndex: 0
+        }
+    }
+
+    incrementCounter = () => {
+        const counter = this.state.currentSlideIndex + 1
+        this.setState({
+            currentSlideIndex: counter
+        })
+    }
+     
+    render() {
+        return (
+            <div onClick={this.incrementCounter}>
+                I am on slide: {this.state.currentSlideIndex}
+            </div>
+        )
+    }
+}
